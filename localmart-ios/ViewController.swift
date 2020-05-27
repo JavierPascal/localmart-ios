cf66373408a7654b6d02fe37c34d026af79cf175
Javier Pascal <A01375925@itesm.mx>
2020-05-27 17:51:16 -0500

    Initial Commit

---
+++ localmart-ios/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  localmart-ios
+//
+//  Created by Javier Pascal Flores on 27/05/20.
+//  Copyright © 2020 Javier Pascal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
