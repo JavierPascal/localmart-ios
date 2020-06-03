b57b5386a4e1afd93ef950285a5f108f422ada75
Javier Pascal <A01375925@itesm.mx>
2020-06-03 15:16:44 -0500

    Communities and profile additions

---
+++ localmart-ios/ViewControllers/CommunitiesViewController.swift
@@ -0,0 +1,22 @@
+//
+//  CommunitiesViewController.swift
+//  localmart-ios
+//
+//  Created by Javier Pascal Flores on 03/06/20.
+//  Copyright © 2020 Javier Pascal. All rights reserved.
+//
+
+import UIKit
+
+class CommunitiesViewController: UIViewController {
+
+    @IBOutlet weak var table: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
